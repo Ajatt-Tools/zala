6c5e467676b04402014adcddd4bbf83fd9855ec0
Ren Tatsumoto <tatsu@autistici.org>
2026-03-01 02:24:09 +0300

    add class RubberBandOptions

--- zala/rubber_band.py
+++ zala/rubber_band.py
@@ -3,9 +3,18 @@ Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 """
 
+import dataclasses
+
 from PyQt6.QtCore import QPoint, QRect, QSize
 from PyQt6.QtGui import QColor, QPainter, QPaintEvent, QPen
 from PyQt6.QtWidgets import QRubberBand, QWidget
+
+
+@dataclasses.dataclass
+class RubberBandOptions:
+    border_thickness: int = 2
+    border_color: QColor = QColor(0, 0, 255)
+    fill_color: QColor = QColor(0, 128, 255, 60)
 
 
 class UserSelectionRubberBand(QRubberBand):
@@ -13,38 +22,35 @@ class UserSelectionRubberBand(QRubberBand):
     Rubber band that is shown when the user selects an area of the pixmap.
     """
 
-    _border_color: QColor = QColor(0, 0, 255)
-    _border_thickness: int = 2
-    _fill_color: QColor = QColor(0, 128, 255, 60)
     _selection_start: QPoint = QPoint()
+    _opts: RubberBandOptions
 
     def __init__(
         self,
         parent: QWidget,
         shape: QRubberBand.Shape = QRubberBand.Shape.Rectangle,
-        thickness=2,
-        border_color: QColor = QColor(0, 0, 255),
-        fill_color=QColor(0, 128, 255, 60),
+        opts: RubberBandOptions | None = None,
     ) -> None:
         super().__init__(shape, parent)
-        self.set_border(border_color, thickness)
-        self.set_fill(fill_color)
+        self._opts = opts or RubberBandOptions()
+        self.set_border(opts.border_color, opts.border_thickness)
+        self.set_fill(opts.fill_color)
         self.hide()
 
     def set_border(self, color: QColor, thickness: int) -> None:
-        self._border_color = color
-        self._border_thickness = thickness
+        self._opts.border_color = color
+        self._opts.border_thickness = thickness
 
     def set_fill(self, color: QColor) -> None:
-        self._fill_color = color
+        self._opts.fill_color = color
 
     def paintEvent(self, event: QPaintEvent) -> None:
         painter = QPainter()
         painter.begin(self)
         # Mask
-        painter.fillRect(event.rect(), self._fill_color)
+        painter.fillRect(event.rect(), self._opts.fill_color)
         # Border
-        painter.setPen(QPen(self._border_color, self._border_thickness))
+        painter.setPen(QPen(self._opts.border_color, self._opts.border_thickness))
         painter.drawRect(event.rect())
         painter.end()
         # return super().paintEvent(event)
